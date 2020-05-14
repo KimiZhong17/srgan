c9df001728dda89bb606ee3adcf5370e54de10b0
HongquanZhang <h4zhang@ucsd.edu>
2020-05-14 16:38:18 -0700

    load_1.1

--- dataloader.py
+++ dataloader.py
@@ -11,9 +11,13 @@ class DataLoader:
         self.FilePath = FilePath
         self.ds = None
         self.train_data = None
+        self.size = 0
         
     def __len__(self):
-        raise NotImplementedError
+        if not self.size:
+            raise NotImplementedError
+        else:
+            return self.size
 
     def get_ds(self,batch_size):
 
@@ -26,6 +30,7 @@ class DataLoader:
         def generator():
             for img in self.ds:
                 img = tf.image.random_crop(img, [192, 192, 3])
+                self.size+=1
                 yield img
 
         DataList = tl.files.load_file_list(path=self.FilePath, regx='.*.png', printable=False)
@@ -37,7 +42,7 @@ class DataLoader:
         self.train_data = self.train_data.batch(batch_size)
         self.train_data = self.train_data.prefetch(buffer_size=AUTOTUNE)
 
-    def show(self):
+    def show_example(self):
         for img in self.train_data:
-            plt.imshow(img)
-
+            plt.imshow(img[0][1]/255.0)
+            plt.imshow(img[0][0] / 255.0)
