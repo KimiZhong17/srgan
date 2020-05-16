8ec673604fefaccb3f68c356c6dc4cf2ac1816a7
maiyuxiaoge <andy1212@126.com>
2020-05-15 20:26:33 -0700

    simplify

--- dataloader.py
+++ dataloader.py
@@ -6,98 +6,55 @@ import matplotlib.pyplot as plt
 import os
 
 class DataLoader:
-    def __init__(self,FilePath,FilePathS,batch_size=1):
+    def __init__(self,sourcePath):
+        self.sourcePath = sourcePath
+        self.data = None
         self.size = None
-        self.FilePath = FilePath
-        self.train_data = None
-        self.train_data_S = None
-        self.labels = None
-        self.hascrop = []
-        self.produce_train(batch_size, FilePath)
-        self.produce_train_S(batch_size, FilePathS)
-        self.produce_labels(FilePath)
-
-
-    def get_train(self):
-        return self.train_data
-
-    def get_train_S(self):
-        return self.train_data_S
-
-    def get_labels(self):
-        return self.labels
-
-    def get_train_P(self):
-        return list(zip(self.get_train(), self.get_train_S()))
 
 
     def __len__(self):
         if self.size is None:
             self.size = 0
-            for _ in os.listdir(self.FilePath):
+            for _ in os.listdir(self.sourcePath):
                 self.size += 1
         else:
             return self.size
 
-    def produce_data(self,thePath):
-        DataList = tl.files.load_file_list(path=thePath, regx='.*.png', printable=False)
-        data = tl.vis.read_images(DataList, path=thePath, n_threads=32)
+
+    def create_data(self,path):
+        DataList = tl.files.load_file_list(path=path, regx='.*.png', printable=False)
+        data = tl.vis.read_images(DataList, path=path, n_threads=32)
         return data
+    
+    def produce(self,batch_size):
+        sources = self.create_data(self.sourcePath)
 
-    def produce_labels(self,thePath):
-        batch_size=1
+        def generator():          
+            for s in sources:
+                yield s
+        
+        def random_edit1(img):
+            source = tl.prepro.zoom(img, zoom_range=(0.25, 0.25))
+            target = tf.convert_to_tensor(img)
+            # source = tf.convert_to_tensor(source)
+            return source,target
 
-        def random_edit(img):
-            return img
+        def random_edit2(img):
+            target = tf.image.random_crop(img, [384, 384, 3])
+            target = target / (255. / 2.)
+            target = target - 1.
+            target = tf.image.random_flip_left_right(target)
+            source = tf.image.resize(target, size=[96, 96])
+            return source,target
 
-        def generator():
-            for img in data:
-                img = tf.convert_to_tensor(img)
-                yield img
-
-        data = self.produce_data(thePath)
-        self.labels = tf.data.Dataset.from_generator(generator, output_types=tf.float32)
-        self.labels = self.labels.map(random_edit, num_parallel_calls=multiprocessing.cpu_count())
-        self.labels = self.labels.batch(batch_size)
-        self.labels = self.labels.prefetch(buffer_size=AUTOTUNE)
-
-
-    def produce_train(self,batch_size,thePath):
-
-        def random_edit(img):
-            # hr_img = tf.image.random_flip_up_down(img)
-            # hr_img = tf.image.random_flip_left_right(hr_img)
-            return img
-
-        def generator():
-            for img in data:
-                img = tf.image.random_crop(img, [192, 192, 3])
-                self.hascrop.append(img)
-                yield img
-
-        data = self.produce_data(thePath)
-        self.train_data = tf.data.Dataset.from_generator(generator, output_types=tf.float32)
-        self.train_data = self.train_data.map(random_edit, num_parallel_calls=multiprocessing.cpu_count())
-        self.train_data = self.train_data.batch(batch_size)
-        self.train_data = self.train_data.prefetch(buffer_size=AUTOTUNE)
-
-
-    def produce_train_S(self,batch_size,thePath):
-
-        def random_edit(img):
-            return img
-
-        def generator():
-            for img in self.hascrop:
-                #img = tf.image.random_crop(img, [192, 192, 3])
-                img = tf.image.resize(img, size=[48, 48])
-                yield img
-
-        self.train_data_S = tf.data.Dataset.from_generator(generator, output_types=tf.float32)
-        self.train_data_S = self.train_data_S.map(random_edit, num_parallel_calls=multiprocessing.cpu_count())
-        self.train_data_S = self.train_data_S.batch(batch_size)
-        self.train_data_S = self.train_data_S.prefetch(buffer_size=AUTOTUNE)
+        self.data = tf.data.Dataset.from_generator(generator, output_types=tf.float32)
+        if batch_size >1:
+            self.data = self.data.map(random_edit2, num_parallel_calls=multiprocessing.cpu_count())
+        else:
+            self.data = self.data.map(random_edit1, num_parallel_calls=multiprocessing.cpu_count())
+        self.data = self.data.batch(batch_size)
+        self.data = self.data.prefetch(buffer_size=AUTOTUNE)
 
     def show_example(self):
-        for img in self.train_data:
+        for img in self.data:
             plt.imshow(img[0]/255.0)
