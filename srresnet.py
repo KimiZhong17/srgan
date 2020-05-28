38d041780e3fef7f3a22cd5249875ae18445e676
maiyuxiaoge <andy1212@126.com>
2020-05-27 21:38:46 -0700

    fixbug

--- srresnet.py
+++ srresnet.py
@@ -12,9 +12,9 @@ class get_G(Model):
         self.conv1 = Conv2d(n_filter=64,filter_size=(3, 3),strides=(1, 1),in_channels=3, act=tf.nn.relu, padding='SAME', W_init=w_init,b_init=None)
         self.conv2 = Conv2d(n_filter=64,filter_size=(3, 3),strides=(1, 1),in_channels=64, padding='SAME', W_init=w_init,b_init=None)
         self.conv3 = Conv2d(n_filter=256,filter_size=(3, 3),strides=(1, 1),in_channels=64, padding='SAME', W_init=w_init,b_init=None)
-        self.conv4 = Conv2d(n_filter=256,filter_size=(3, 3),strides=(1, 1),in_channels=256, padding='SAME', W_init=w_init,b_init=None)
-        self.conv5 = Conv2d(n_filter=3,filter_size=(3, 3),strides=(1, 1),in_channels=256, act=tf.nn.tanh, padding='SAME', W_init=w_init,b_init=None)
-        self.bn1 = BatchNorm2d(num_features = 64,gamma_init=g_init)
+        self.conv4 = Conv2d(n_filter=3,filter_size=(3, 3),strides=(1, 1),in_channels=64, act=tf.nn.tanh, padding='SAME', W_init=w_init,b_init=None)
+        self.bn2 = BatchNorm2d(num_features = 64,gamma_init=g_init)
+        self.bn1 = BatchNorm2d(num_features = 64,gamma_init=g_init, act=tf.nn.relu)
         self.subconv1 = SubpixelConv2d(scale=2, n_out_channels=256,in_channels=256, act=tf.nn.relu)
         self.add1 = Elementwise(tf.add)
 
@@ -25,21 +25,19 @@ class get_G(Model):
             x2 = self.conv2(x)
             x2 = self.bn1(x2)
             x2 = self.conv2(x2)
-            x2 = self.bn1(x2)
+            x2 = self.bn2(x2)
             x2 = self.add1([x,x2])
             x = x2
         
         x = self.conv2(x)
         x = self.bn1(x)
-        x = self.ea1([x,x1])
-
+        x = self.add1([x,x1])
+        x = self.conv3(x)
+        x = self.subconv1(x)
         x = self.conv3(x)
         x = self.subconv1(x)
 
         x = self.conv4(x)
-        x = self.subconv1(x)
-
-        x = self.conv5(x)
 
         return x 
         
@@ -58,7 +56,7 @@ class get_D(Model):
         self.conv3 = Conv2d(n_filter=df_dim * 4,filter_size=(4, 4),strides=(2, 2),in_channels=df_dim* 2, padding='SAME', W_init=w_init, b_init=None)
         self.bn2 =  BatchNorm2d(num_features = df_dim * 4, act=lrelu,gamma_init=g_init)
         self.conv4 = Conv2d(n_filter=df_dim * 8,filter_size=(4, 4),strides=(2, 2),in_channels=df_dim* 4, padding='SAME', W_init=w_init, b_init=None)
-        self.bn3 =  BatchNorm2d(num_features = df_dim * 28, act=lrelu,gamma_init=g_init)
+        self.bn3 =  BatchNorm2d(num_features = df_dim * 8, act=lrelu,gamma_init=g_init)
         self.conv5 = Conv2d(n_filter=df_dim * 16,filter_size=(4, 4),strides=(2, 2),in_channels=df_dim* 8, padding='SAME', W_init=w_init, b_init=None)
         self.bn4 =  BatchNorm2d(num_features = df_dim * 16, act=lrelu,gamma_init=g_init)
         self.conv6 = Conv2d(n_filter=df_dim * 32,filter_size=(4, 4),strides=(2, 2),in_channels=df_dim* 16, padding='SAME', W_init=w_init, b_init=None)
@@ -71,41 +69,41 @@ class get_D(Model):
         self.bn8 =  BatchNorm2d(num_features = df_dim * 2, act=lrelu,gamma_init=g_init)
         self.conv10 = Conv2d(n_filter=df_dim * 2,filter_size=(3, 3),strides=(1, 1),in_channels=df_dim* 2, padding='SAME', W_init=w_init, b_init=None)
         self.bn9 =  BatchNorm2d(num_features = df_dim * 2, act=lrelu,gamma_init=g_init)
-        self.conv11 = Conv2d(n_filter=df_dim * 8,filter_size=(3, 3),strides=(1, 1),in_channels=df_dim* 8, padding='SAME', W_init=w_init, b_init=None)
-        self.bn10 =  BatchNorm2d(num_features = df_dim * 8, act=lrelu,gamma_init=g_init)
-        self.add1 = Elementwise(tf.add)
+        self.conv11 = Conv2d(n_filter=df_dim * 8,filter_size=(3, 3),strides=(1, 1),in_channels=df_dim* 2, padding='SAME', W_init=w_init, b_init=None)
+        self.bn10 =  BatchNorm2d(num_features = df_dim * 8,gamma_init=g_init)
+        self.add1 = Elementwise(combine_fn=tf.add, act=lrelu)
         self.flat1 = Flatten()
-        self.dence1 = Dense(n_units=1, W_init=w_init, in_channels=df_dim * 8)
+        self.dence1 = Dense(n_units=1, W_init=w_init, in_channels= 72*df_dim)
 
 
 
-        def forward(self,x):
-            x = self.conv1(x)
-            x = self.conv2(x)
-            x = self.bn1(x)
-            x = self.conv3(x)
-            x = self.bn2(x)
-            x = self.conv4(x)
-            x = self.bn3(x)
-            x = self.conv5(x)
-            x = self.bn4(x)
-            x = self.conv6(x)
-            x = self.bn5(x)
-            x = self.conv7(x)
-            x = self.bn6(x)
-            x = self.conv8(x)
-            x1 = self.bn7(x)
-            x = self.conv9(x1)
-            x = self.bn8(x)
-            x = self.conv10(x)
-            x = self.bn9(x)
-            x = self.conv11(x)
-            x = self.bn10(x)
-            x = self.add1([x,x1])
-            x = self.flat1(x)
-            x = self.dence1(x)
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = self.conv4(x)
+        x = self.bn3(x)
+        x = self.conv5(x)
+        x = self.bn4(x)
+        x = self.conv6(x)
+        x = self.bn5(x)
+        x = self.conv7(x)
+        x = self.bn6(x)
+        x = self.conv8(x)
+        x1 = self.bn7(x)
+        x = self.conv9(x1)
+        x = self.bn8(x)
+        x = self.conv10(x)
+        x = self.bn9(x)
+        x = self.conv11(x)
+        x = self.bn10(x)
+        x = self.add1([x,x1])
+        x = self.flat1(x)
+        x = self.dence1(x)
 
-            return x
+        return x
 
 
 
--- train_srresnet.py
+++ train_srresnet.py
@@ -11,8 +11,7 @@ def train(
     G,
     batch_size,
     n_epoch,
-    learning_rate,
-    tds=None):
+    learning_rate):
     n_step_epoch = round(n_epoch // batch_size)
     G = get_G()
     D = get_D()
@@ -20,6 +19,8 @@ def train(
     g_optimizer_init = tf.optimizers.Adam(learning_rate)
     g_optimizer = tf.optimizers.Adam(learning_rate)
     d_optimizer = tf.optimizers.Adam(learning_rate)
+    writer = tf.summary.create_file_writer('./log/'+ name)
+    
     G.train()
     D.train()
     VGG.train()
@@ -62,7 +63,10 @@ def train(
                 "Epoch: [{}/{}] step: [{}/{}] time: {:.3f}s, g_loss(mse:{:.3f}, vgg:{:.3f}, adv:{:.3f}) d_loss: {:.3f}".format(
                     epoch, n_epoch, step, n_step_epoch, time.time() - step_time, mse_loss, vgg_loss, g_gan_loss,
                     d_loss))
-
+        if epoch >0:
+            with writer.as_default():
+                tf.summary.scalar('D_loss', g_loss, step=epoch+1)
+                tf.summary.scalar('G_loss', d_loss, step=epoch+1)
     tl.files.exists_or_mkdir('models')
     G.save_weights('./models/g.h5')
     D.save_weights('./models/d.h5')
@@ -72,9 +76,9 @@ if __name__ == '__main__':
 
     ##############################################
     ##########      hyperparamter   ##############
-    batch_size = 8
+    batch_size = 2
     learning_rate = 0.01
-    n_epoch = 50
+    n_epoch = 1
     name = 'srres_1'
     ##############################################
     trainloader = DataLoader('../srgan/DIV2K_train_HR/')
